30af79ad129a8412f0913edaae2bc37c9d509ddc
bmotadev <bmotadev@gmail.com>
2022-08-30 18:06:15 -0300

    usando indexof

--- StringsBuscas/Program.cs
+++ StringsBuscas/Program.cs
@@ -9,7 +9,8 @@ namespace StringsBuscas
             #region Busca
             var outroParagrafo = "C# é uma linguagem moderna e versátil. " + "Com C# consigo desenvolver para Web, Desktop, Jogos, " + "Mobile, entre outros.";
 
-            // IndexOf
+            // IndexOf = retorna o indice da primeira ocorrencia
+            var indexOf = outroParagrafo.IndexOf("C#");
             #endregion
 
             Console.ReadKey();
