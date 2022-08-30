a3cb0606c8a304f6bd7cf41197485d617404ac90
bmotadev <bmotadev@gmail.com>
2022-08-30 12:27:16 -0300

    Add project files.

---
+++ PrincipaisMetodos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrincipaisMetodos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var frase = "C# é uma linguagem moderna e versátil. " + "Com C# consigo desenvolver para Web, Desktop, Jogos, " + "Mobile, entre outros.";
+        }
+    }
+}
