5d8a382dd837503338f4279b0c27d034939935b0
bmotadev <bmotadev@gmail.com>
2022-08-30 18:25:37 -0300

    Metodos de Buscas concluido

--- StringsBuscas/Program.cs
+++ StringsBuscas/Program.cs
@@ -11,6 +11,21 @@ namespace StringsBuscas
 
             // IndexOf = retorna o indice da primeira ocorrencia
             var indexOf = outroParagrafo.IndexOf("C#");
+
+            // LastIndexOf = retorna o indice da ultima ocorrencia
+            var lastIndexOf = outroParagrafo.LastIndexOf("C#");
+
+            // StartsWith = retorna o valor verdadeiro ou falso, confirmando se o resultado é o esperado.
+            var startsWith = outroParagrafo.StartsWith("C#");
+
+            // Substring
+            var indexOfMobile = outroParagrafo.IndexOf("Mobile");
+            var substringMobile = outroParagrafo.Substring(indexOfMobile, 6); //"Mobile".Length para  saber o tamanho, no caso 6.
+
+            // Contains
+            var containsJogo = outroParagrafo.Contains("jogos", StringComparison.OrdinalIgnoreCase);
+            var containsJogosExact = outroParagrafo.Contains("Jogos");
+            var containsRuim = outroParagrafo.Contains("ruim");
             #endregion
 
             Console.ReadKey();
